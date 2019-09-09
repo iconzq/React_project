543d607d4480960dd79dcb9a4db78bae3c352b53
zhangqian <iconqian@163.com>
2019-09-09 19:14:57 +0800

    更新

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import React, {Component} from 'react'
+import {BrowserRouter as Router,Route,} from 'react-router-dom'
+import routes from './config/routes'
 
 export default class App extends Component {
   constructor(props) {
@@ -7,9 +9,14 @@ export default class App extends Component {
 
   render() {
     return (
-        <div>
-          App..
-        </div>
+        <Router>
+          {/* exact 严格匹配，只匹配path里的路由 */}
+          {
+            routes.map((route,index) => {
+              return <Route {...route} key={index}/>
+            })
+          }
+        </Router>
     )
   }
 }
---
+++ src/redux/reducers.js
@@ -0,0 +1,13 @@
+/*根据之前的prevState和action来生成新的状态*/
+import {combineReducers} from 'redux'
+
+function xxx(prevState = {}, action) {
+  switch (action.type) {
+    default:
+      return prevState
+  }
+}
+
+export default combineReducers({
+  xxx
+})
