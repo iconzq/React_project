260f1608e46117ef6f3843099b4efcb49c3e13fb
zhangqian <iconqian@163.com>
2019-09-11 11:04:28 +0800

    完成用户登录界面和简单的用户名、密码校验功能

--- src/components/login/index.jsx
+++ src/components/login/index.jsx
@@ -1,15 +1,83 @@
 import React, {Component} from 'react'
+import logo from './logo.png'
+import {Form, Icon, Input, Button, Checkbox} from 'antd';
+import './index.less'
 
-export default class Login extends Component {
+@Form.create()
+class Login extends Component {
   constructor(props) {
     super(props)
   }
 
+  validator = (rule, value, callback) => {
+    const name = rule.file === 'username' ? '用户名' : '密码'
+    const reg = /^[a-zA-Z0-9],{4,16}$/
+    if (!value) {
+      return callback(`请输入${name}`)
+    }
+    if (value.length < 3) {
+      return callback(`${name}最小长度是3位`)
+    }
+    if (value.length > 16) {
+      return callback(`${name}最大长度是16位`)
+    }
+
+    if (!reg.test(value)) {
+      return callback(`${name}必须是数字、字母、下划线，不能存在非法字符`)
+    }
+    callback()
+  }
+
   render() {
+    const {getFieldDecorator} = this.props.form
     return (
-        <div>
-          Login...
+        <div className="login">
+          <header className="login-header">
+            <img src={logo} alt="logo"/>
+            <h1>React后台管理系统</h1>
+          </header>
+          <section className="login-section">
+            <h2>用户登录</h2>
+            <Form>
+              <Form.Item>
+                {getFieldDecorator(
+                    'username',
+                    {
+                      rules: [
+                        /*{required: true, message: '请输入用户名'},
+                        {min: 3, message: '最小长度是3位'},
+                        {max: 16, message: '最大长度16位'},
+                        {pattern: /^[a-zA-Z0-9],{3,13}$/, message: '用户名必须是数字、字母、下划线，存在非法字符'}*/
+                        {validator: this.validator}
+                      ]
+                    }
+                )
+                (
+                    <Input type="text" prefix={<Icon type="user"/>}/>
+                )}
+
+              </Form.Item>
+              <Form.Item>
+                {getFieldDecorator(
+                    'password',
+                    {
+                      rules: [
+                        {validator: this.validator}
+                      ]
+                    }
+                )
+                (
+                    <Input type="password" prefix={<Icon type="lock"/>}/>
+                )}
+              </Form.Item>
+              <Form.Item>
+                <Button type="submit" className="formBtn">登录</Button>
+              </Form.Item>
+            </Form>
+          </section>
         </div>
     )
   }
 }
+
+export default Login
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDom from 'react-dom'
 import {Provider} from 'react-redux'
 import store from './redux/store'
 import App from './App'
+import './assets/less/index.less'
 ReactDom.render(<Provider store={store}>
   <App/>
 </Provider> ,document.getElementById('root'))
